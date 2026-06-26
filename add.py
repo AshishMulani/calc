46a7873f144313fd9d0f3f49be6b55fcb0ba16ff
PGCP-AI <mulaniashish16@gmail.com>
2026-06-26 09:59:14 +0530

    Multiplication added

--- add.py
+++ add.py
@@ -2,3 +2,4 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 print(f"Result of Addition: {num1 + num2}")
+print(f"Result of Multiplication: {num1 * num2}")
